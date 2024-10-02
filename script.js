8500cb6b9ca8e888b03c685d645c515486ddb919
Nuffyola <nurulfaiziahm@gmail.com>
2024-10-03 02:10:44 +0800

    Half done

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// Toggle Button
 
-const menuBtn = document.querySelector('#menuBtn');
-const navBar = document.querySelector('#navBar');
-
-menuBtn.addEventListener('click', () => {
-  if (navBar.style.display === 'none' || navBar.style.display === '') {
-    navBar.style.display = 'block';
-  } else {
-    navBar.style.display = 'none';
-  }
+document.getElementById("toggleButton").addEventListener("click", function() {
+    document.body.classList.toggle("collapsed");
 });
+
+
